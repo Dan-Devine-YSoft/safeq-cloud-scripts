a2e06e539748433e3165a346801bf6c42717c442
Dan Devine <dan.devine@ysoft.com>
2024-05-08 13:18:54 +1000

    Initial commit

---
+++ register_current_session_user.ps1
@@ -0,0 +1,12 @@
+# Input API key for the given customer that has advanced permissions [CreateUser, ViewUser]
+# and [Allow unauthenticated requests, Allow Unauthorized Endpoints] set on the key
+$headers = @{
+    "X-Api-Key" = "API_KEY"
+}
+
+# Input customer domain
+$apiUrl = "https://CUSTOMER_DOMAIN:7300/api/v1/users"
+
+$currentUsername = [Environment]::UserName
+$response = Invoke-RestMethod -Uri $apiUrl -Method Put -Headers $headers -Body "username=$currentUsername" -ContentType "application/x-www-form-urlencoded" -SkipCertificateCheck
+Write-Output $response
